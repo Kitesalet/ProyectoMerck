2cd9b6717dffbe0add3b278002f1da4c806265de
Kitesalet <104630744+Kitesalet@users.noreply.github.com>
2023-12-04 17:59:34 -0300

    ltm

--- ProyectoMerck/wwwroot/js/site.js
+++ ProyectoMerck/wwwroot/js/site.js
@@ -2,3 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+    var map = null;
+    function GetMap() {
+        map = new Microsoft.Maps.Map(document.getElementById('mapper'), {
+            credentials: 'Ans7MhY4N2wow6lyrbE9gL7eXhZJAjTIp76MkysYSWln1kmnzP43uDzPT_jd7yHu'
+        });
+    }
+
+    GetMap();
+
+
+
+document.addEventListener('DOMContentLoaded', GetMap)
